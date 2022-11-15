6087a28315d2f9f31113fd0cfffc7c3e0f64eb95
Allan Okoth <allanokothdev@gmail.com>
2022-11-16 00:16:45 +0200

    configuring scripts

--- contract/scripts/deploy.sh
+++ contract/scripts/deploy.sh
@@ -4,6 +4,6 @@ source ./scripts/setting.conf
 
 near delete $SUB_ACCOUNT $MASTER_ACCOUNT 
 
-near create-account $SUB_ACCOUNT --masterAccount $MASTER_ACCOUNT --initialBalance 50
+near create-account $SUB_ACCOUNT --masterAccount $MASTER_ACCOUNT --initialBalance 10
 
 near deploy $SUB_ACCOUNT --wasmFile=./res/mint_condition.wasm
