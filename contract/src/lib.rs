00254f871822d9ac5f0ba10eb113590754042be0
Allan Okoth <allanokothdev@gmail.com>
2022-11-17 17:31:18 +0200

    I commit to you

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -17,7 +17,7 @@ pub struct Contract {
     metadata: LazyOption<NFTContractMetadata>,
 }
 
-const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
+const MINT_CONDITION_ICON: &str = "";
 
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
@@ -39,8 +39,8 @@ impl Contract {
             NFTContractMetadata {
                 spec: NFT_METADATA_SPEC.to_string(),
                 name: "MINT Condition Non-Fungible Token".to_string(),
-                symbol: "MINT-CO".to_string(),
-                icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
+                symbol: "MINT".to_string(),
+                icon: None,
                 base_uri: None,
                 reference: None,
                 reference_hash: None,
@@ -63,13 +63,7 @@ impl Contract {
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
         }
     }
-
-    /// Mint a new token with ID=`token_id` belonging to `receiver_id`.
-    ///
-    /// Since this example implements metadata, it also requires per-token metadata to be provided
-    /// in this call. `self.tokens.mint` will also require it to be Some, since
-    /// `StorageKey::TokenMetadata` was provided at initialization.
-    ///
+    
     /// `self.tokens.mint` will enforce `predecessor_account_id` to equal the `owner_id` given in
     /// initialization call to `new`.
     #[payable]
---
+++ contract/target/debug/deps/mint_condition-940dd8b34e69ab0b.d
@@ -0,0 +1,5 @@
+/home/allanokothdev/mint-condition/contract/target/debug/deps/mint_condition-940dd8b34e69ab0b: src/lib.rs
+
+/home/allanokothdev/mint-condition/contract/target/debug/deps/mint_condition-940dd8b34e69ab0b.d: src/lib.rs
+
+src/lib.rs:
