559f1d73bf76f26fc933863cb8cdde0935bc75d7
Allan Okoth <allanokothdev@gmail.com>
2022-11-17 18:56:18 +0200

    Call.sh operations

--- contract/scripts/call.sh
+++ contract/scripts/call.sh
@@ -2,11 +2,11 @@
 
 source ./scripts/setting.conf
 
-near call $ID new_default_meta '{"owner_id": "'$ID'"}' --accountId $ID
+near call $SUB_ACCOUNT  new_default_meta '{"owner_id": "'$SUB_ACCOUNT '"}' --accountId $SUB_ACCOUNT 
 
-near view $ID nft_metadata
+near view $SUB_ACCOUNT  nft_metadata
 
-near call $ID nft_mint '{"token_id": "0", "receiver_id": "'$ID'", "token_metadata": { "title": "Some Art", "description": "My NFT media", "media": "https://bafkreiabag3ztnhe5pg7js4bj6sxuvkz3sdf76cjvcuqjoidvnfjz7vwrq.ipfs.dweb.link/", "copies": 1}}' --accountId $ID --deposit 0.1
+near call $SUB_ACCOUNT  nft_mint '{"token_id": "0", "receiver_id": "'$SUB_ACCOUNT '", "token_metadata": { "title": "Some Art", "description": "My NFT media", "media": "https://bafkreiabag3ztnhe5pg7js4bj6sxuvkz3sdf76cjvcuqjoidvnfjz7vwrq.ipfs.dweb.link/", "copies": 1}}' --accountId $SUB_ACCOUNT  --deposit 0.1
 
-near view $ID nft_tokens_for_owner '{"account_id": "'$ID'"}'
+near view $SUB_ACCOUNT  nft_tokens_for_owner '{"account_id": "'$SUB_ACCOUNT '"}'
 
